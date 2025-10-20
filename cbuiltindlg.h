0c7a17f2f35b8549752bf7a7f52fdb4b77e981ae
ID41343107 <41343107@nfu.edu.tw>
2025-10-20 16:24:41 +0800

    Project Created!

---
+++ cbuiltindlg.h
@@ -0,0 +1,33 @@
+#ifndef CBUILTINDLG_H
+#define CBUILTINDLG_H
+
+#include <QDialog>
+#include <QPushButton>
+#include <QTextEdit>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class cbuiltindlg;
+}
+QT_END_NAMESPACE
+
+class cbuiltindlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    cbuiltindlg(QWidget *parent = nullptr);
+    ~cbuiltindlg();
+
+private:
+    QTextEdit* displayTextEdit;
+    QPushButton* colorPushBtn;
+    QPushButton* errorPushBtn;
+    QPushButton* filePushBtn;
+    QPushButton* fontPushBtn;
+    QPushButton* inputPushBtn;
+    QPushButton* pagePushBtn;
+    QPushButton* progressPushBtn;
+    QPushButton* printPushBtn;
+};
+#endif // CBUILTINDLG_H
